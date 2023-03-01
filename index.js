1cfd19d7a64c7231bd4b730704e8835a0fd99566
GGrop <giuliano.gropponi@gmail.com>
2023-03-01 08:19:57 -0300

    set $error

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const $board = document.querySelector('.gridWrapper');
 const $buttonStart = document.querySelector('#start')
 let $initialSquare = null
 let errors = 0
+const $errors = document.querySelector('#errors')
+$errors.textContent = errors
