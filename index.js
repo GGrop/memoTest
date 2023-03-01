26b2efe7c15c2f0e3ad7d0f8203ddd289dd66ec6
GGrop <giuliano.gropponi@gmail.com>
2023-03-01 08:25:37 -0300

    add the configuration of the shuffled colors in squares

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ $buttonStart.onclick=function(){
     startGame()
 }
 
+const configureGame=()=>{
+    $buttonStart.classList.add('hide');
+    const shuffledColors = handleColors()
+    handleRandomBoard(shuffledColors)
+}
+const handleColors=()=>{
+    const colorsClass = ['redSquare', 'purpleSquare', 'pinkSquare','yellowSquare', 'greenSquare', 'blueSquare']
+    const pairColorsClass = colorsClass.concat(colorsClass)
+    pairColorsClass.sort(function(){
+        return 0.5 - Math.random()
+    })
+    return pairColorsClass
+}
+const handleRandomBoard=(shuffledColors)=>{
+    const $squares = document.querySelectorAll('#square')
+    $squares.forEach(function($square,i){
+        $square.classList = shuffledColors[i]
+    })
+}
+
